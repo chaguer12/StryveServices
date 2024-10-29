2a76eeb28bb9e87a8b0ca037f01f4d9072c153bc
zoolotoui <chag.med.amine@gmail.com>
2024-10-29 11:39:23 +0100

    stat: fixing devlopper problem

--- StryveAPI/services/TaskService.js
+++ StryveAPI/services/TaskService.js
@@ -1,6 +1,8 @@
 const tasks = require('../models/tasks')
+const developpers = require('../models/developers')
 
 class TaskService{
+    
     static getTasks(){
         return tasks;
     }
@@ -8,6 +10,32 @@ class TaskService{
     static addTask(task){
         tasks.push(task);
     }
+    static assignTasksWithPriorityAndDependencies(tasks,developpers){
+        const assignedTasks = [];
+        const unassignedTasks = [];
+        const devTasks = developpers.map(dev => ({
+            name: dev.name,
+            assignedTasks: [],
+            hours: 0
+        }))
+        const checkDependencies = (task, assignedTasks) =>{
+            return task.dependencies.every(dep => assignedTasks.some(assigned => assigned.taskName === dep));
+        }
+        tasks.sort((a,b) => b.priority - a.priority);
+        for(const task of tasks){
+            if(task.dependencies.length >0 && !checkDependencies(task, assignedTasks)){
+                unassignedTasks.push(task);
+                continue;
+            }
+        }
+        const findDevelopper = developpers.find(dev =>{
+            dev.skillLevel >= task.difficulty && dev.maxHours >= task.hoursRequired && dev.preferredTaskType === task.taskType
+        });
+        if(findDevelopper){
+            const developperAssignment = developperAssignment.find(dev => dev.name === findDevelopper.name);
+        }
+    }
+
 }
 
 module.exports = TaskService;  
